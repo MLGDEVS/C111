b002b827b62ff3cb09c507d99b9aa3c023043675
MLGDEVS <109704226+MLGDEVS@users.noreply.github.com>
2023-05-19 10:16:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ prediction1 = ""
 prediction2 = ""
 
 Webcam.set({
-  width: 350,
-  height: 300,
-  imageFormat: 'png',
-  pngQuality: 90
-});
-
-camera = document.getElementById("camera");
-Webcam.attach("#camera");
-      
-function takeSnapshot() 
-{
-  Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML = '<img id="captured_image" src="+data_uri+"/>';
-    
+    width:350,
+    height:300,
+    imageFormat : 'png',
+    pngQuality:90
   });
 
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+      
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
 }
 
   console.log('ml5 version:', ml5.version);
